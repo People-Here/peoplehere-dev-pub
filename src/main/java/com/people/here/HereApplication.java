8a6932ef35a27679f3e27155ea2bdc6816eec213
kuhi <tkddls4456@naver.com>
2024-06-06 15:08:38 +0900

    test commit

---
+++ src/main/java/com/people/here/HereApplication.java
@@ -0,0 +1,13 @@
+package com.people.here;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/people/here/HereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.here;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
